2bf44889837f35d085f9a1f50faa0131a8ac90a4
Bibhuti Bhsan Sahoo <63660520+Bibhuti5@users.noreply.github.com>
2022-03-14 09:35:54 +0530

    spring mvc test program

---
+++ mvc_test/src/main/java/com/cg/mvc/MvcTestApplication.java
@@ -0,0 +1,13 @@
+package com.cg.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcTestApplication.class, args);
+	}
+
+}
---
+++ mvc_test/src/test/java/com/cg/mvc/MvcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
